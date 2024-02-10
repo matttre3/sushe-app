43acdd4114e80446ce986d49ea1ca2b56db7d40e
matttre3 <140153086+matttre3@users.noreply.github.com>
2024-02-10 19:26:04 +0100

    vercel.json modificato

--- sushe_app/src/App.jsx
+++ sushe_app/src/App.jsx
@@ -1,69 +1,53 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Init from './components/Init'
-import Register from './components/Register';
-import Joincreate from './components/joincreate';
-import Joinpin from './components/Joinpin';
-import MyOrder from './components/MyOrder';
-import AllOrder from './components/AllOrder';
-import './App.css'
-
-
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Init from "./components/Init";
+import Register from "./components/Register";
+import Joincreate from "./components/Joincreate";
+import Joinpin from "./components/Joinpin";
+import MyOrder from "./components/MyOrder";
+import AllOrder from "./components/AllOrder";
+import "./App.css";
 
 function App() {
-  const [userName, setUserName] = useState('')
-  const [password, setPassword] = useState('')
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <>
       <Routes>
-        <Route path="/" element={
-          <Init
-            userName={userName}
-            setUserName={setUserName}
-            password={password}
-            setPassword={setPassword}
-          />
-        }
+        <Route
+          path="/"
+          element={
+            <Init
+              userName={userName}
+              setUserName={setUserName}
+              password={password}
+              setPassword={setPassword}
+            />
+          }
         />
-        <Route path="/register" element={
-          <Register
-            userName={userName}
-            setUserName={setUserName}
-            password={password}
-            setPassword={setPassword}
-          />
-        }
-        />
-
-        <Route path="/joincreate" element={
-          <Joincreate
-          />
-        }
+        <Route
+          path="/register"
+          element={
+            <Register
+              userName={userName}
+              setUserName={setUserName}
+              password={password}
+              setPassword={setPassword}
+            />
+          }
         />
 
-        <Route path="/joinpin" element={
-          <Joinpin
-          />
-        }
-        />
+        <Route path="/joincreate" element={<Joincreate />} />
 
+        <Route path="/joinpin" element={<Joinpin />} />
 
-        <Route path="/myorder" element={
-          <MyOrder
-          />
-        }
-        />
+        <Route path="/myorder" element={<MyOrder />} />
 
-        <Route path="/allorder" element={
-          <AllOrder
-          />
-        }
-        />
-
+        <Route path="/allorder" element={<AllOrder />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
